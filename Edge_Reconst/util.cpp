3c334ecffecdc01505c621749ca827b973846100
C-H-Chien <chiangheng.chien@gmail.com>
2025-08-18 01:38:05 -0400

    Remove OpenCV dependency for the preparation of the final code version

--- Edge_Reconst/util.cpp
+++ Edge_Reconst/util.cpp
@@ -364,50 +364,6 @@ namespace MultiviewGeometryUtil {
         }
         return reproj_errs;
     }
-
-    std::string multiview_geometry_util::cvMat_Type(int type)
-    {
-        //> Credit: https://stackoverflow.com/questions/10167534/how-to-find-out-what-type-of-a-mat-object-is-with-mattype-in-opencv
-
-        std::string r;
-
-        uchar depth = type & CV_MAT_DEPTH_MASK;
-        uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-        switch (depth)
-        {
-        case CV_8U:
-            r = "8U";
-            break;
-        case CV_8S:
-            r = "8S";
-            break;
-        case CV_16U:
-            r = "16U";
-            break;
-        case CV_16S:
-            r = "16S";
-            break;
-        case CV_32S:
-            r = "32S";
-            break;
-        case CV_32F:
-            r = "32F";
-            break;
-        case CV_64F:
-            r = "64F";
-            break;
-        default:
-            r = "User";
-            break;
-        }
-
-        r += "C";
-        r += (chans + '0');
-
-        return r;
-    }
-
 }
 
 #endif
